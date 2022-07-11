23cf2d25d6c5d8ae81011d19d2e90dd8c0d4ac5d
Abass Adamo <adamoabasstope@yahoo.com>
2022-07-11 09:47:27 +0100

    setup container with most water

---
+++ LeetCode/Medium/container_with_most_water.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ var maxArea = function(height) {
+    console.log(height)
+};
+
+const height = [1,8,6,2,5,4,8,3,7]
+// const height = [1,8,6,2,5,4,8,3]
+
+maxArea(height);
+
